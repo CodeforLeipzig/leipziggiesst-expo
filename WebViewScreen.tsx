4fa64cf25f7f861d457e3f01080108faf33e9501
Georg Hans <gehans@outlook.de>
2022-09-21 16:52:34 +0200

    init

---
+++ WebViewScreen.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const whiteList = [
+  "https://giessdeinviertel.codeforleipzig.de",
+  "https://leipziggiesst-login.codeforleipzig.de",
+];
+
+const WebViewScreen = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <WebView
+      style={[styles.webView, { marginTop: insets.top }]}
+      source={{ uri: "https://giessdeinviertel.codeforleipzig.de" }}
+      originWhitelist={whiteList}
+      allowsBackForwardNavigationGestures
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  webView: {
+    flex: 1,
+  },
+});
+
+export default WebViewScreen;
