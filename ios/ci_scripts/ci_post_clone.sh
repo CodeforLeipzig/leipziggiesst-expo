995b9cdb10ca90d34e01034bdbd5c27e80da7417
Jörg Reichert <joergreichert@users.noreply.github.com>
2023-11-12 13:25:52 +0100

    adapt script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -14,7 +14,7 @@
  # Install yarn and pods dependencies.
  # If you're using Flutter or Swift 
  # just install pods by "pod install" command 
- ls && cd .. && yarn && pod install
+ ls && cd ../.. && npm install && npm run ioss
 
  echo "🎯 Stage: Post-clone is done .... "
 
